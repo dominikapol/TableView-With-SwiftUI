2b777571afcea09bd1474be5c38f2245cea666a9
vladislav <vlad.shtytcko@lookout.com>
2021-11-17 19:03:25 +0300

    Initial Commit

---
+++ Lection34/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Lection34
+//
+//  Created by Vladislav on 17.11.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Lection34/Lection34App.swift
@@ -0,0 +1,17 @@
+//
+//  Lection34App.swift
+//  Lection34
+//
+//  Created by Vladislav on 17.11.21.
+//
+
+import SwiftUI
+
+@main
+struct Lection34App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
